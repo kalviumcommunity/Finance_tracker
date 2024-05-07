b98acf6fbe3b31ca0b4c48af114085eb9c8ef5f0
Itachi-789 <vighneshzagade4@gmail.com>
2024-05-07 10:40:37 +0530

    Schema Initialization and controllers setup

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -1,20 +1,43 @@
-// @desc to GET all the Transactions 
-// @route GET /api/v1/transactions
-// @access public
-exports.getTransactions = (req, res, next) => {
-    res.send("Get Transactions!!")
-}
+const Transaction = require("../models/transaction");
+
+// @desc get all transactions
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+    res
+      .status(200)
+      .json({ success: true, count: transactions.length, data: transactions });
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Server Error" });
+  }
+};
+exports.deleteTransaction = async (req, res, next) => {
+    try {
+      const removedTransaction = await Transaction.findByIdAndRemove(req.params.id);
+      if (!removedTransaction) {
+        return res.status(404).json({ success: false, error: "No transaction found" });
+      }
+      res.status(200).json({ success: true, data: removedTransaction });
+    } catch (error) {
+      res.status(500).json({ success: false, error: "Server Error", message: error.message });
+    }
+  };
 
 // @desc to add a transaction
-// @route POST /api/v1/transactions
-// @access public
-exports.deleteTransaction = (req, res, next) => {
-    res.send("DELETE Transactions!!")
-}
-
-// @desc to delete a transaction
-// @route DELETE /api/v1/transactions
-// @access public
-exports.addTransaction = (req, res, next) => {
-    res.send("POST Transactions!!")
-}
+exports.addTransaction = async (req, res, next) => {
+  const { text, amount } = req.body;
+  try {
+    const transaction = await Transaction.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: transaction,
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const messages = Object.values(error.errors).map((val) => val.message);
+      return res.status(400).json({ success: false, error: messages });
+    } else {
+      return res.status(500).json({ success: false, error: "Server Error" });
+    }
+  }
+};
